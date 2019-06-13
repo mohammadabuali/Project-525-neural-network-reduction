bbb33fc936605ba02c2914d3c846013a1158ae6c
mohammadabuali <51571630+mohammadabuali@users.noreply.github.com>
2019-06-13 13:37:50 +0300

    HelloWorld.py
    A simple Hello World written in tensorflow enviroment

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+
+hello = tf.constant('Hello, TensorFlow!')
+
+with tf.Session() as sess:
+    print(sess.run(hello).decode())
